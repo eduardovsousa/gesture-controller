700a2d03f9f03da74d73ae056764deeccf6da455
Eduardo Varela Sousa <eduardo.varela@outlook.com.br>
2023-03-02 14:20:20 -0300

    Adicionando um novo reconhecimento de gesto

--- pages/titles/src/util/gestures.js
+++ pages/titles/src/util/gestures.js
@@ -2,6 +2,7 @@ const { GestureDescription, Finger, FingerCurl } = window.fp;
   
 const ScrollDownGesture = new GestureDescription('scroll-down'); // ✊️
 const ScrollUpGesture = new GestureDescription('scroll-up'); // 🖐
+const RockGesture = new GestureDescription('rock'); // 🤘
 
   
 // Scroll down
@@ -27,15 +28,28 @@ for(let finger of Finger.all) {
     ScrollUpGesture.addCurl(finger, FingerCurl.NoCurl, 1.0);
 }
 
+// Rock
+// -----------------------------------------------------------------------------
+
+RockGesture.addCurl(Finger.Thumb, FingerCurl.NoCurl , 1.0);
+RockGesture.addCurl(Finger.Index, FingerCurl.NoCurl , 1.0);
+RockGesture.addCurl(Finger.Pink, FingerCurl.NoCurl , 1.0);
+
+for (let finger of [Finger.Thumb, Finger.Index, Finger.Pinky]) {
+  RockGesture.addCurl(finger, FingerCurl.FullCurl, 1.0)
+  RockGesture.addCurl(finger, FingerCurl.HalfCurl, 0.9)
+}
 
 const knowGestures = [
   ScrollDownGesture,
   ScrollUpGesture,
+  RockGesture,
 ]
 
 const gestureStrings = {
   'scroll-up': '🖐',
-  'scroll-down': '✊️'
+  'scroll-down': '✊️',
+  'rock': '🤘'
 }
 
 export {
